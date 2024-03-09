14bf92266194ab4567355962fdac77417dcf239c
fjmartinez0 <fj.martinez0@gmail.com>
2024-03-09 00:20:19 -0500

    Added function avg days for a price change

--- second_try.py
+++ second_try.py
@@ -50,19 +50,28 @@ try:
                 game_price_history = game_price_history_jsonObj['data']
 
                 my_curated_price_list = []
+                my_price_change_dates = []
                 for price_point in game_price_history:
+                    current_price_point = [price_point[0], price_point[-1]]
+
                     #print("what is this" + price_point[0]) 
                     #print(type(price_point))
                     if not my_curated_price_list:
-                        my_curated_price_list.append(price_point)
+                        #my_curated_price_list.append(price_point)
+                        my_curated_price_list.append(current_price_point)
+                        my_price_change_dates.append(current_price_point[0])
+                        #print(my_curated_price_list)
                         #print(price_point)
                     else:
-                        if my_curated_price_list[-1][1:] == price_point[1:]:
+                        #print(price_point[0], )
+                        #if it is ONLY the same price as before, skip record, otherwise, append
+                        if my_curated_price_list[-1][-1:] == current_price_point[-1:]: 
                             continue
                         else:
-                            my_curated_price_list.append(price_point)
+                            my_curated_price_list.append(current_price_point)
+                            my_price_change_dates.append(current_price_point[0])
                             #print(price_point)
-                this_game_details.append([game_release_date, game_platforms, my_curated_price_list])
+                this_game_details.append([game_release_date, game_platforms, my_curated_price_list, my_price_change_dates])
                 return this_game_details
             
                 #print(price_point[1:]) # this is an array of price points at a given date
@@ -70,14 +79,19 @@ try:
             
             more_game_details = get_title_details(game_url)
             game_price_history = more_game_details[0][2]
+            game_price_changes_dates_history = more_game_details[0][3]
+            
+            print("I mam here")
+            print(more_game_details[0][3])
             #print(more_game_details[0][2])
 
-            
+
             # How long it takes for the first discount
             def time_for_first_discount(game_price_history):
                 print(len(game_price_history))
                 if(len(game_price_history)>=2):
                     #print(1)
+                    print(len(game_price_history))
                     print("First price: " + game_price_history[0][0])
                     print("First discount: " + game_price_history[1][0])
                     d1 = datetime.strptime(game_price_history[0][0], "%Y-%m-%d")
@@ -86,8 +100,54 @@ try:
                 else:
                     return 0
                 
+            def discounts_analysis(game_price_history, game_price_changes_dates_history):
+                print("======= Discounts Analysis =========")
+                print(len(game_price_history))
+                discounts_analsysis_result = []
+                if(len(game_price_history)>=2):
+                    #print(1)
+                    #print(game_price_history[0:][0])
+                    date_list = game_price_changes_dates_history
+                    #print(date_list)
+                    # Convert date strings to datetime objects
+                    datetime_list = [datetime.strptime(date_str, "%Y-%m-%d") for date_str in date_list]
+                    #print (len(game_price_changes_dates_history))
+                    total_days = 0
+                    # Calculate differences
+                    for i in range(len(game_price_changes_dates_history) - 1):
+                        #print(i)
+                        difference = (datetime_list[i + 1] - datetime_list[i]).days
+                        total_days += difference
+                        print(difference)
+                        #print(f"Difference between {date_list[i]} and {date_list[i + 1]}: {difference}")
+                    my_avg_days_for_price_change = total_days/(len(game_price_changes_dates_history) - 1)
+                    print (f"Avg days for price change:  {my_avg_days_for_price_change}")
+
+                    for discount in game_price_history:
+                        price_change_date = discount[-1]
+                        price_change_value = discount[-1]
+                        d1 = datetime.strptime(game_price_history[0][0], "%Y-%m-%d")
+                        d2 = datetime.strptime(game_price_history[1][0], "%Y-%m-%d")
+
+                        #print(discount)
+                        #print("here")
+                        #print(discount[0][0] + " " + discount[0][-1] )
+                        #print(len(discount))
+                        #print(discount[1][0] + " " + discount[1][-1] )
+                        #print(len(game_price_history))
+                        #print("First price: " + game_price_history[0][0])
+                        #print("First discount: " + game_price_history[1][0])
+                        #d1 = datetime.strptime(game_price_history[0][0], "%Y-%m-%d")
+                        #d2 = datetime.strptime(game_price_history[1][0], "%Y-%m-%d")
+                        #discounts_analsysis_result.append(abs((d2 - d1).days))
+                    return discounts_analsysis_result
+                else:
+                    return 0
+                
             days_for_first_discount = time_for_first_discount(game_price_history)
-            print(days_for_first_discount)
+            print("Days for first discount: " + str(days_for_first_discount))
+
+            discounts_analysis(game_price_history, game_price_changes_dates_history)
 
             # How often it gives a discount
                             
